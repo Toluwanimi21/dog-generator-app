8ad4ca090d3a30c2aedf6ce1513026ae700452f5
Nimi <ademaratibunmi@gmail.com>
2023-05-30 20:52:09 +0100

    successfully implemented

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function getDogImage() {
     let image = document.getElementById("image");
     fetch("https://dog.ceo/api/breeds/image/random")
-        .then(response => response.json())
-        .then((json) => console.log(json))
+        .then(response => {
+            console.log(response)
+            return response.json()
+        })
+        .then(data => {
+                image.innerHTML = `<img src="${data.message}">`;
+        })
+
+        // .then(response => response.json())
+        // .then((json) => console.log(json))
     // .then(response  =>  console.log(response)) 
     //   console.log(response)
     // .then(json => image.src = json[0].url)
